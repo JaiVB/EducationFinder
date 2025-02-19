e8cd82325c0dd47f6a45a311f571e962c8dae189
JamesCHamilton <165865052+JamesCHamilton@users.noreply.github.com>
2025-02-19 18:13:16 -0500

    gitignore update

---
+++ frontend/app/edufind/page.tsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import { useState } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./components/dropdowns";
